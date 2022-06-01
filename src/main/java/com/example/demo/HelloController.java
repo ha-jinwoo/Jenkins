b6014340edcd52d14657b323eeed2181438d6e86
ha-jinwoo <gkwlsdn95@naver.com>
2022-06-02 01:14:40 +0900

    두번째 jar파일 생성

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,4 +9,9 @@ public class HelloController {
     public String hello() {
         return "Hello Aurora3~!!!";
     }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "Hi jenkins";
+    }
 }
